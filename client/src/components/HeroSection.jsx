4d1d032ebf44bbf9de682e210cd4a109f44261c5
TechJackk <techjack2815@gmail.com>
2025-03-15 18:38:41 +0530

    updated hover and used Arial

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -11,19 +11,16 @@ const HeroSection = () => {
                 Super Charge your Agile Workflow!<MoveRight size={23} color="#a40ff3" />
             </a>
         </div>
-        <h1 className="mt-13 text-3xl lg:text-6xl tracking-wide font-bold text-center">
-            Accelerate your Agile Streamline your success!!!
-            {/* <span className="bg-gradient-to-r from-[#a40ff3] to-purple-400 text-transparent bg-clip-text">
-                {" "}
-            </span> */}
+        <h1 className="font-[Arial] mt-13 text-3xl lg:text-6xl tracking-wide font-bold text-center">
+            Accelerate your Agile<br/> Streamline your success!!!
         </h1>
         <p className='mt-13 text-center text-neutral-500 max-w-4xl'>
         Stay ahead with SprintX, the ultimate Scrum project management tool. Plan, track progress,
          and collaborate effortlessly-all in one place. Boost your productivity by 16X.
         </p>
         <div className="flex flex-col lg:flex-row justify-center my-13 gap-5 lg:gap-20">
-            <a href="#" className="inline-flex items-center gap-2 text-black border-n shadow-md bg-[#a40ff3] text-white rounded-r-full rounded-l-full px-3 py-1">Get Started<span><ArrowRight size={25} color="#ffffff" /></span></a>
-            <a href="#" className="inline-flex items-center gap-2 text-black border-n rounded-r-full rounded-l-full shadow-lg px-3 py-1"><CirclePlay size={32} color="#000000" />Watch Demo</a>
+            <a href="#" className="inline-flex items-center gap-2 text-black border-n shadow-md bg-[#a40ff3] text-white rounded-r-full rounded-l-full px-3 py-1 hover:bg-white hover:text-[#a40ff3] transition-colors duration-200 group">Get Started<span><ArrowRight size={25} className="text-white group-hover:text-[#a40ff3] transition-colors duration-200" /></span></a>
+            <a href="#" className="inline-flex items-center gap-2 text-black border-n rounded-r-full rounded-l-full shadow-lg px-3 py-1 hover:bg-[#a40ff3] hover:text-white transition-colors duration-200 group"><CirclePlay size={32} className="text-black group-hover:text-white transition-colors duration-200" />Watch Demo</a>
         </div>
         <img src={template} alt="dashboard" className="w-full max-w-4xl mx-auto rounded-2xl shadow-lg object-cover"/>
     </div>
