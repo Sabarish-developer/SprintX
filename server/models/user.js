535323e1f1aa1a8d5f58e127f197568a347def3b
Sabarish-developer <sabarish.cspro@gmail.com>
2025-03-22 11:32:00 +0530

    chore(schema): update user schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {type: String},
-    email: {type:String, unique: true},
-    password: {type: String},
-    role: {type: String, enum: ["Team member", "Scrum master", "Product owner", "Admin"]},
+    username: {type: String, required: true},
+    email: {type:String, unique: true, required: true},
+    password: {type: String, required: true},
+    role: {type: String, enum: ["Team member", "Scrum master", "Product owner", "Admin"], default: "Team member"},
     subrole: {type: String}
 })
 
