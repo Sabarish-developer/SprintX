c11d5b031c14ae4ec7aec37e225c08d4fcfaf16c
TechJackk <techjack2815@gmail.com>
2025-03-23 19:09:06 +0530

    faded footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-      <footer className="bg-white w-full text-black py-4 mt-10 border-t border-gray-300">
-        <div className="text-center text-sm">
+      <footer className="bg-white w-full text-black py-4 mt-10 border-t border-gray-300 bg-gradient-to-r from-purple-500 to-purple-200">
+        <div className="text-white text-center text-sm">
           &copy; {new Date().getFullYear()} <span className="font-semibold">SprintX</span> - All rights reserved.
         </div>
       </footer>
