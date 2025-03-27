ef809b1003d056eb3b0426f732692f8d99ac3d6e
Sabarish-developer <sabarish.cspro@gmail.com>
2025-03-28 00:23:48 +0530

    feat(app): add cors for hosting

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from "cors";
 import errorHandler from './middleware/errorHandler.js';
 import authRouter from './routes/authRoute.js';
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
