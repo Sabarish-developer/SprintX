095742fe9eca602831374e3c3ae4664ca6a828b3
Sabarish-developer <sabarish.cspro@gmail.com>
2025-03-30 19:37:14 +0530

    chore(cors): update cors to allow flight requests

--- server/index.js
+++ server/index.js
@@ -8,8 +8,20 @@ import authRouter from './routes/authRoute.js';
 dotenv.config();
 const app = express();
 app.use(cors({
-    origin: process.env.CLIENT_URL
+    origin: process.env.CLIENT_URL,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"]
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
