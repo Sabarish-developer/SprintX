c896b9877000db0f5d361300fd914fdafa5de6fe
Sabarish-developer <sabarish.cspro@gmail.com>
2025-03-30 13:37:47 +0530

    feat(cors): set client_url in origin of cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ import authRouter from './routes/authRoute.js';
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
