e2bda49c191ccfaee7a20a69efa3fc04c99cae18
TechJackk <techjack2815@gmail.com>
2025-04-04 21:22:45 +0530

    outlets centered

--- client/src/components/DashboardLayout.jsx
+++ client/src/components/DashboardLayout.jsx
@@ -17,7 +17,7 @@ export default function DashboardLayout() {
       </SideBar>
 
       {/* Main Content */}
-      <div className="ml-16 flex-1 overflow-x-auto">
+      <div className="flex-1 overflow-x-auto">
         <Outlet />
       </div>
     </div>
