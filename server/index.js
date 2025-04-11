cc450278b891576e22ed7aafbae6b2775dff562e
Sabarish-developer <sabarish.cspro@gmail.com>
2025-04-11 19:18:49 +0530

    chore(index): update cors setup

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import teamMemberRouter from './routes/teamMemberRoute.js';
 
 dotenv.config();
 const app = express();
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
