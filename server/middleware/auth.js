0e0a4b4d40343e1848638cff7648a94faa3d4dd6
Sabarish-developer <sabarish.cspro@gmail.com>
2025-04-11 18:45:52 +0530

    chore(auth): update headers

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const auth = (req,res,next)=>{
 
-    const token = req.headers.authorization;
+    const token = req.headers.Authorization;
     
     if(!token){
         return res.status(401).json({message: "Invalid user request."});
