5b04402426696bccf212c0a36666f45a95e523d6
TechJackk <techjack2815@gmail.com>
2025-04-15 21:42:39 +0530

    refactor(projects): project card onClick change to name to avoid mis clicks

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -315,11 +315,11 @@ export default function Projects() {
         onConfirm={ async() => {
           try {
            const response = await axios.delete(`${import.meta.env.VITE_BASE_URL}/api/productowner/projects`, {
-  data: { projectId },
-  headers: {
-    Authorization: token
-  }
-});
+              data: { projectId },
+              headers: {
+                Authorization: token
+              }
+            });
 
             if (response.status === 200) {
               // Remove the deleted project from local state
@@ -392,7 +392,7 @@ export default function Projects() {
           </div>
           {isProductOwner &&
           <button
-          onClick={() => setShowModal(true)}
+          onClick={() => {setShowModal(true); setProjectToEdit(null); setSelectedScrumMaster(null); setSelectedTeamMembers([]);}}
           className="bg-[#a40ff3] hover:bg-white cursor-pointer hover:text-[#a40ff3] text-white px-4 py-2 rounded shadow hover:shadow-md text-sm flex items-center gap-2 w-40"
           >
             <FolderPlus size={18} />
@@ -754,7 +754,7 @@ export default function Projects() {
       </div>
 
       {/* Project Cards */}
-      <div className="bg-purple-50 py-10 flex flex-wrap w-auto h-auto justify-center gap-10 px-10 lg:px-0" onClick={() => navigate(`/home/tasks`)}>
+      <div className="bg-purple-50 py-10 flex flex-wrap w-auto h-auto justify-center gap-10 px-10 lg:px-0">
       {error ? (
           <div className="text-red-500 text-center">{error}</div>
         ) : projects.length > 0 ? (
@@ -763,10 +763,10 @@ export default function Projects() {
             {projects.map((project) => (
               <div
                 key={project.id}
-                className={`border-0 rounded-md shadow w-60 md:w-80 lg:w-80 p-5 cursor-pointer hover:shadow-lg sm:w-1/2 md:w-1/3 lg:w-1/5 transition duration-200 bg-white ${getCardBg(project.progress)}`}
+                className={`border-0 rounded-md shadow w-60 md:w-80 lg:w-80 p-5 hover:shadow-lg sm:w-1/2 md:w-1/3 lg:w-1/5 transition duration-200 bg-white ${getCardBg(project.progress)}`}
               >
                 <div className="flex items-center justify-between gap-2 mb-2 w-full">
-                  <div className="w-4/4 min-w-0 cursor-pointer">
+                  <div className="w-4/4 min-w-0 cursor-pointer" onClick={() => navigate(`/home/tasks`)}>
                     <h2 className="text-xl font-bold mb-2 overflow-hidden whitespace-nowrap text-ellipsis">
                       {project.name}
                     </h2>
